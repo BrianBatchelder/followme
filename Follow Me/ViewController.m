60267dd82d55fe59558923326170b27db776eace
Brian Batchelder <brian@briansbrain.biz>
2014-11-15 10:05:46 -0800

    Initial Commit

---
+++ Follow Me/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Follow Me
+//
+//  Created by Brian Batchelder on 11/15/14.
+//  Copyright (c) 2014 Brian's Brain. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Follow MeTests/Follow_MeTests.m
@@ -0,0 +1,40 @@
+//
+//  Follow_MeTests.m
+//  Follow MeTests
+//
+//  Created by Brian Batchelder on 11/15/14.
+//  Copyright (c) 2014 Brian's Brain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Follow_MeTests : XCTestCase
+
+@end
+
+@implementation Follow_MeTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
